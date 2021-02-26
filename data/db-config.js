e11aa35c680a4dad99b74de40e06971d294940c1
Chris <chris@databoxdigital.com>
2021-02-26 15:58:47 -0600

    knex connection file

--- data/db-config.js
+++ data/db-config.js
@@ -1,5 +1,5 @@
 require('dotenv').config();
 const knex = require('knex');
 const dbConfig = process.env.DB_ENV || 'development';
-const knexConfig = require('../../knexfile')[dbConfig];
+const knexConfig = require('../knexfile')[dbConfig];
 module.exports = knex(knexConfig);
