c76d430b0ba396f42e8048c83af5101c52b2853f
Chris <chris@databoxdigital.com>
2021-03-02 14:44:00 -0600

    accept query params on dashboard

--- api/dashboard/dashboardRouter.js
+++ api/dashboard/dashboardRouter.js
@@ -2,9 +2,11 @@ var express = require('express');
 var router = express.Router();
 var DB = require('../../data/dbInterface');
 var { auth } = require('../middleware/auth');
+var { validateQuery } = require('../middleware/validate');
 
-router.get('/:table', auth, function (req, res) {
+router.get('/:table', validateQuery, auth, function (req, res) {
   DB.findBy(req.params.table, {
+    ...res.locals.query,
     [`${req.params.table}.owner_id`]: res.locals.user.id,
   })
     .then((objs) => {
